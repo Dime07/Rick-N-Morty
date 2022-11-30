993e5f8b5c38ecb3a97b8421995d7094db15b810
firdiajadeh <65402012+firdiajadeh@users.noreply.github.com>
2022-11-30 21:52:36 +0700

    Add files via upload

---
+++ src/pages/search/backend.js
@@ -0,0 +1,54 @@
+  // Note: deps array kosong [] buat
+    // this useEffect jalan sekali
+    function App() {
+        const [error, setError] = useState(null);
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [items, setItems] = useState([]);
+
+        useEffect(() => {
+            fetch("https://rickandmortyapi.com/api/character")
+                .then((res) => res.json())
+                .then(
+                    (result) => {
+                        setIsLoaded(true);
+                        setItems(result);
+                    },
+                    
+                    (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                );
+        }, []);
+
+        if (error) {
+            return <>{error.message}</>;
+        } else if (!isLoaded) {
+            return <>loading...</>;
+        } else {
+            return (
+                /* here we map over the element and display each item as a card  */
+                <div className="wrapper">
+                    <ul className="card-grid">
+                        {items.map((item) => (
+                            <li>
+                                <article className="card" key={item.callingCodes}>
+                                    <div className="card-image">
+                                        <img src={item.flag} alt={item.name} />
+                                    </div>
+                                    <div className="card-content">
+                                        <h2 className="card-name">{item.name}</h2>
+                                        <ol className="card-list">
+                                            
+                                        </ol>
+                                    </div>
+                                </article>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
+    }
+
+    ReactDOM.render(<App />, document.getElementById("root"));
--- src/pages/search/client.js
+++ src/pages/search/client.js
@@ -1,15 +1,54 @@
-import ".app.css";
+return items.filter((item) => {
+    return searchParam.some((newItem) => {
+        return (
+            item[newItem]
+                .toString()
+                .toLowerCase()
+                .indexOf(q.toLowerCase()) > -1
+        );
+    });
+});
 
-function app() {
-    return (
-        <div classname="app">
-            <input type="text" placeholder="Search whatever" classname="search" />
-            <ul classname="list">
-                <li classname="listItem">Rick</li>
-                <li className="ListItems">Morty</li>
-                <li className="list">Summer</li>
-                <li className="list">Beth</li>
-            </ul>
-        </div>
-    );
-}
+return <>{error.message}</>;
+          if (!isLoaded) {
+            return <>loading...</>;
+        } else {
+            return (
+                <div className="wrapper">
+                    <div className="search-wrapper">
+                        <label htmlFor="search-form">
+                            <input
+                                type="search"
+                                name="search-form"
+                                id="search-form"
+                                className="search-input"
+                                placeholder="Search for..."
+                                value={q}
+                                /*
+                                // nentuin value usestate
+                                //  tiap kali user ngetik di search bar
+                                */
+                                onChange={(e) => setQ(e.target.value)}
+                            />
+                            <span className="sr-only">Search Characters</span>
+                        </label>
+                    </div>
+                    <ul className="card-grid">
+                        {items.map((item) => (
+                            <li>
+                                <article className="card" key={item.callingCodes}>
+                                    <div className="card-image">
+                                        <img src={item.flag} alt={item.name} />
+                                    </div>
+                                    <div className="card-content">
+                                    </div>
+                                </article>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
+    
+
+    ReactDOM.render(<App />, document.getElementById("root"));

7d10268beb5b15960fb8c4872252cf5a1ea053fb
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2022-12-07 15:21:07 +0700

    name correction

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,60 +1,32 @@
-export default function About(){
-    return(
-        <section style={{display: 'flex', flexDirection: 'column', padding: '20px 210px', textAlign: 'center'}}>
-            <div style={{margin: 'auto'}}>
-                <h1>
-                    Rick and morty website
-                </h1>
-                <p>
-                    website ini merupakan website yang menggunakan dua api yaitu api rick and morty dan reqres .Dibuat oleh kelompok 30, dengan anggota kelompok :
-                </p>
-                <table style={{margin:'0 auto'}}>
-                    <tr>
-                        <td>
-                            Helmy Zakiudin
-                        </td>
-                        <td>
-                            :
-                        </td>
-                        <td>
-                            21120119130040
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Dimas Rafi
-                        </td>
-                        <td>
-                            :
-                        </td>
-                        <td>
-                            21120119120022
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Chairullatif Aji Sadewa
-                        </td>
-                        <td>
-                            :
-                        </td>
-                        <td>
-                            21120119120015
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            MUHAMMAD ALAM FIRDAUS
-                        </td>
-                        <td>
-                            :
-                        </td>
-                        <td>
-                            21120119140147
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </section>
-    )
+export default function About() {
+  return (
+    <section style={{ display: 'flex', flexDirection: 'column', padding: '20px 210px', textAlign: 'center' }}>
+      <div style={{ margin: 'auto' }}>
+        <h1>Rick and morty website</h1>
+        <p>website ini merupakan website yang menggunakan dua api yaitu api rick and morty dan reqres .Dibuat oleh kelompok 30, dengan anggota kelompok :</p>
+        <table style={{ margin: '0 auto' }}>
+          <tr>
+            <td>HELMY ZAKIUDIN</td>
+            <td>:</td>
+            <td>21120119130040</td>
+          </tr>
+          <tr>
+            <td>DIMAS RAFI</td>
+            <td>:</td>
+            <td>21120119120022</td>
+          </tr>
+          <tr>
+            <td>CHAIRULLATIF AJI SADEWA</td>
+            <td>:</td>
+            <td>21120119120015</td>
+          </tr>
+          <tr>
+            <td>MUHAMMAD ALAM FIRDAUS</td>
+            <td>:</td>
+            <td>21120119140147</td>
+          </tr>
+        </table>
+      </div>
+    </section>
+  );
 }
